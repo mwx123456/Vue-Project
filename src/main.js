532d146561ec4d8daca3e3b170eba8cdb5999301
unknown <993457988@qq.com>
2019-01-16 15:29:50 +0800

    993457988@qq.com

---
+++ src/main.js
@@ -0,0 +1,24 @@
+//入口文件
+
+import Vue from "vue";
+
+
+import {Header} from "mint-ui";
+Vue.component(Header.name,Header);
+
+import "./lib/mui/css/mui.min.css"
+
+
+
+import app from "./App.vue"
+
+
+
+
+var vm = new Vue({
+  el:"#app",
+  render:c => c(app)
+
+
+
+})
---
+++ src/router.js
@@ -0,0 +1,13 @@
+import VueRouter from 'vue-router'
+
+
+// 3. 创建路由对象
+var router = new VueRouter({
+  routes: [
+    // account  goodslist
+        
+  ]
+})
+
+// 把路由对象暴露出去
+export default router
